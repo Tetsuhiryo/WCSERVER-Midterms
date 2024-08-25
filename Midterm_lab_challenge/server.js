0ac76cfd73219d9030539bff8def4bd29662867f
Tetsuhiryo <156308083+Tetsuhiryo@users.noreply.github.com>
2024-08-25 15:57:36 +0800

    Add files via upload

---
+++ Midterm_lab_challenge/server.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var app = express();
+
+var bodyParser = require('body-parser');
+
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + "/" + "index.html");
+})
+
+app.post('/process_post', urlencodedParser, function (req, res) {
+  response = {
+    first_name: req.body.first_name,
+    last_name: req.body.last_name,
+    email: req.body.email,
+    phone: req.body.phone,
+    gender: req.body.gender,
+    hobbies: req.body.hobbies
+  };
+
+  console.log(response);
+  res.send(JSON.stringify(response));
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Listening on Port ${port}`);
+});
